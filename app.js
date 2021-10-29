b81019717c18f3fb856359e73fd4854ce7bb702b
Albert Chap <chapal@flip2.engr.oregonstate.edu>
2021-10-28 21:24:51 -0700

    initial code

---
+++ app.js
@@ -0,0 +1,29 @@
+// App.js
+
+/*
+    SETUP
+*/
+const express = require('express');   // We are using the express library for the web server
+const app     = express();            // We need to instantiate an express object to interact with the server in our code
+const PORT    = 7575;                 // Set a port number at the top so it's easy to change in the future
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+/*
+    ROUTES
+*/
+// app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
+//     {
+//         res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
+//     });                                         // requesting the web site.
+
+app.use(express.static('public'));
+
+/*npm
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
