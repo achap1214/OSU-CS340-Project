d339b2ce89b723cf652f6cffd3af7d1358550591
Austin Chap <chapa@flip2.engr.oregonstate.edu>
2021-11-11 20:14:36 -0800

    Added data manipulation file

---
+++ data_manipulation_file.sql
@@ -0,0 +1,50 @@
+
+
+/*Traders*/
+--Query for adding a new trader functionality 
+INSERT INTO `Traders`(`TraderID`,`TraderFirstName`, `TraderLastName`) 
+VALUES (:traderIDInput, :traderFnameInput, :traderLnameInput) ;
+
+--Query for deleting a trader
+DELETE FROM `Traders` WHERE `TraderID`= :traderIDselected;
+
+--Query for updating a specific Trader
+UPDATE `Traders` 
+SET `TraderID` = :traderIDInput, `TraderFirstName` = :traderFnameInput, `TraderLastName` = :traderLnameInput
+WHERE `TraderID` = :traderIDselected;
+
+--Query for gettting all trader names
+SELECT * FROM `Traders`;
+
+/*Managers*/
+--Query for adding a new trader functionality 
+INSERT INTO `Managers`(`ManagerID`,`ManagerFirstName`, `ManagerLastName`) 
+VALUES (:managerIDInput, :managerFnameInput, :managerLnameInput) ;
+
+--Query for deleting a manager
+DELETE FROM `Managers` WHERE `ManagerID`= :managerIDselected;
+
+--Query for updating a specific manager
+UPDATE `Managers` 
+SET `ManagerID` = :managerIDInput, `ManagerFirstName` = :managerFnameInput, `ManagerLastName` = :managerLnameInput
+WHERE `ManagerID` = :managerIDselected;
+
+--Query for gettting all trader names
+SELECT * FROM `Managers`;
+
+
+/*Brokers*/
+--Query for adding a new trader functionality 
+INSERT INTO `Brokers`(`BrokerID`,`BrokerName`, `BrokerAddress`) 
+VALUES (:brokerIDInput, :brokerNameInput, :brokerAddressInput) ;
+
+--Query for deleting a broker
+DELETE FROM `Brokers` WHERE `BrokerID` = :brokerIDselected;
+
+--Query for updating a specific broker
+UPDATE `Brokers` 
+SET `BrokerID` = :brokerIDInput, `BrokerName` = :brokerNameInput, `BrokerAddress` = :brokerAddressInput
+WHERE `BrokerID` = :brokerIDselected;
+
+--Query for gettting all trader names
+SELECT * FROM `Brokers`;
