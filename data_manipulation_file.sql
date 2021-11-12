c1c6ef5b676fee4a3a234b6fef3b6a96a488a61d
Albert Chap <chapal@flip2.engr.oregonstate.edu>
2021-11-11 20:34:53 -0800

    added securities, trades, and fills data manipulation

--- data_manipulation_file.sql
+++ data_manipulation_file.sql
@@ -48,3 +48,52 @@ WHERE `BrokerID` = :brokerIDselected;
 
 --Query for getting all trader names
 SELECT * FROM `Brokers`;
+
+/*Securities*/
+--Query for adding a new trader functionality 
+INSERT INTO `Securities`(`SecuritySymbol`,`CompanyName`) 
+VALUES (:securitySymbolInput, :companyNameInput) ;
+
+--Query for deleting a security
+DELETE FROM `Securities` WHERE `SecuritySymbol` = :securitySymbolselected;
+
+--Query for updating a specific security
+UPDATE `Securities` 
+SET `SecuritySymbol` = :securitySymbolInput, `CompanyName` = :companyNameInput
+WHERE `SecuritySymbol` = :securitySymbolselected;
+
+--Query for getting all trader names
+SELECT * FROM `Securities`;
+
+/*Trades*/
+--Query for adding a new trader functionality 
+INSERT INTO `Trades`(`TradeID`,`SecuritySymbol`, `Amount`,`TraderID`, `ManagerID`, `BrokerID`,`Time`, `Date`) 
+VALUES (:tradeIDInput, :securitySymbolInput, :amountInput, :traderIDInput, :managerIDInput, :brokerIDInput, :timeInput, :dateInput) ;
+
+--Query for deleting a broker
+DELETE FROM `Trades` WHERE `TradeID` = :tradeIDselected;
+
+--Query for updating a specific broker
+UPDATE `Trades` 
+SET `TradeID` = :tradeIDInput, `SecuritySymbol` = :securitySymbolInput, `Amount`=:amountInput,`TraderID`=:traderIDInput, `ManagerID`=:managerIDInput, `BrokerID`=:brokerIDInput,`Time`=:timeInput, `Date`=:dateInput
+WHERE `TradeID` = :tradeIDselected;
+
+--Query for getting all trader names
+SELECT * FROM `Trades`;
+
+
+/*Fills*/
+--Query for adding a new trader functionality 
+INSERT INTO `Fills`(`FillID`, `TradeID`,`SecuritySymbol`, `Amount`,`Time`, `Date`) 
+VALUES (:fillsIDInput, :tradeIDInput, :securitySymbolInput, :amountInput, :timeInput, :dateInput) ;
+
+--Query for deleting a broker
+DELETE FROM `Fills` WHERE `FillID` = :fillIDselected;
+
+--Query for updating a specific broker
+UPDATE `Fills` 
+SET `FillID`=:fillIDInput, `TradeID`=:tradeIDInput `SecuritySymbol`=:securitySymbolInput, `Amount`=:amountInput,`Time`=:timeInput, `Date`=:dateInput
+WHERE `FillID` = :fillIDselected;
+
+--Query for getting all trader names
+SELECT * FROM `Fills`;
