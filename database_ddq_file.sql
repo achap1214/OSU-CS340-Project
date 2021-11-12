b51f6add8dcce00c425015127c9beddc2e0000b7
Austin Chap <chapa@flip2.engr.oregonstate.edu>
2021-11-11 19:04:34 -0800

    Updated database ddq file to fix errors

--- database_ddq_file.sql
+++ database_ddq_file.sql
@@ -23,11 +23,11 @@ DROP TABLE IF EXISTS `Traders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Traders` (
-  `TraderID` int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-  `TraderFirstName` varchar(255) NOT UNIQUE NOT NULL,
-  'TraderLastName' varchar(255) NOT UNIQUE NOT NULL,
+  `TraderID` int(11) UNIQUE NOT NULL,
+  `TraderFirstName` varchar(255) NOT NULL,
+  `TraderLastName` varchar(255) NOT NULL,
   PRIMARY KEY (`TraderID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `Traders` (
 LOCK TABLES `Traders` WRITE;
 /*!40000 ALTER TABLE `Traders` DISABLE KEYS */;
 INSERT INTO `Traders` VALUES (1,'James','McAvoy'),(2,'Michael','Fassbender'),(3,'Patrick','Stewart');
-/*!40000 ALTER TABLE `bsg_cert` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Traders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -48,11 +48,11 @@ DROP TABLE IF EXISTS `Managers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Managers` (
-  `ManagerID` int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-  `ManagerFirstName` varchar(255) NOT UNIQUE NOT NULL,
-  'ManagerLastName' varchar(255) NOT UNIQUE NOT NULL,
+  `ManagerID` int(11) UNIQUE NOT NULL,
+  `ManagerFirstName` varchar(255) NOT NULL,
+  `ManagerLastName` varchar(255) NOT NULL,
   PRIMARY KEY (`ManagerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,9 +60,9 @@ CREATE TABLE `Managers` (
 --
 
 LOCK TABLES `Managers` WRITE;
-/*!40000 ALTER TABLE `Traders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Managers` DISABLE KEYS */;
 INSERT INTO `Managers` VALUES (1,'Michael','Scott'),(2,'Steve','Carrell'),(3,'John','Krasinski');
-/*!40000 ALTER TABLE `bsg_cert` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Managers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -73,11 +73,11 @@ DROP TABLE IF EXISTS `Brokers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Brokers` (
-  `BrokerID` int(11) UNIQUE NOT NULL AUTO_INCREMENT,
+  `BrokerID` int(11) UNIQUE NOT NULL,
   `BrokerName` varchar(255) UNIQUE NOT NULL,
-  'BrokerAddress' varchar(255) UNIQUE NOT NULL,
+  `BrokerAddress` varchar(255) NOT NULL,
   PRIMARY KEY (`BrokerID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,9 +85,9 @@ CREATE TABLE `Brokers` (
 --
 
 LOCK TABLES `Brokers` WRITE;
-/*!40000 ALTER TABLE `Traders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Brokers` DISABLE KEYS */;
 INSERT INTO `Brokers` VALUES (1,'Goldman Sachs','NYC'),(2,'JP Morgan','NYC'),(3,'Morgan Stanley','NYC');
-/*!40000 ALTER TABLE `bsg_cert` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Brokers` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -98,10 +98,10 @@ DROP TABLE IF EXISTS `Securities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Securities` (
-  `SecuritySymbol` varchar(255) UNIQUE NOT NULL AUTO_INCREMENT,
+  `SecuritySymbol` varchar(255) UNIQUE NOT NULL,
   `CompanyName` varchar(255) UNIQUE NOT NULL,
   PRIMARY KEY (`SecuritySymbol`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,9 +109,9 @@ CREATE TABLE `Securities` (
 --
 
 LOCK TABLES `Securities` WRITE;
-/*!40000 ALTER TABLE `Traders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Securities` DISABLE KEYS */;
 INSERT INTO `Securities` VALUES ('AAPL','Apple'),('AMZN','Amazon'),('MSFT','Microsoft');
-/*!40000 ALTER TABLE `bsg_cert` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Securities` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -122,19 +122,19 @@ DROP TABLE IF EXISTS `Trades`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Trades` (
-  'TradeID' int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-  'SecuritySymbol' varchar(255) NOT UNIQUE NOT NULL,
-  'Amount' int(11) NOT UNIQUE NOT NULL,
-  'TraderID' int(11) NOT UNIQUE NOT NULL,
-  'ManagerID' int(11) NOT UNIQUE NOT NULL,
-  'BrokerID' int(11) NOT UNIQUE NOT NULL,
-  'Time' Time, NOT UNIQUE, NOT NULL,
-  'Date' Date, NOT UNIQUE, NOT NULL,
+  `TradeID` int(11) UNIQUE NOT NULL,
+  `SecuritySymbol` varchar(255) NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `TraderID` int(11) NOT NULL,
+  `ManagerID` int(11) NOT NULL,
+  `BrokerID` int(11) NOT NULL,
+  `Time` Time NOT NULL,
+  `Date` Date NOT NULL,
   PRIMARY KEY (`TradeID`),
-  CONSTRAINT 'TradesSecuritySymbol' FOREIGN KEY (`SecuritySymbol`) REFERENCES `Securities` (`SecuritySymbol`),
-  CONSTRAINT 'TradesTraderID' FOREIGN KEY (`TraderID`) REFERENCES `Traders` (`TraderID`),
-  CONSTRAINT 'TradesManagerID' FOREIGN KEY (`ManagerID`) REFERENCES `Managers` (`ManagerID`),
-  CONSTRAINT 'TradesBrokerID' FOREIGN KEY (`BrokerID`) REFERENCES `Brokers` (`BrokerID`)
+  CONSTRAINT `TradesSecuritySymbol` FOREIGN KEY (`SecuritySymbol`) REFERENCES `Securities` (`SecuritySymbol`),
+  CONSTRAINT `TradesTraderID` FOREIGN KEY (`TraderID`) REFERENCES `Traders` (`TraderID`),
+  CONSTRAINT `TradesManagerID` FOREIGN KEY (`ManagerID`) REFERENCES `Managers` (`ManagerID`),
+  CONSTRAINT `TradesBrokerID` FOREIGN KEY (`BrokerID`) REFERENCES `Brokers` (`BrokerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -144,6 +144,7 @@ CREATE TABLE `Trades` (
 
 LOCK TABLES `Trades` WRITE;
 /*!40000 ALTER TABLE `Trades` DISABLE KEYS */;
+INSERT INTO `Trades` VALUES (1,'AAPL','1000',1,2,1,'9:30:20','01-01-2021'),(2,'AMZN','500',2,1,2,'9:35:25','01-01-2021'),(3,'MSFT','750',1,1,1,'9:37:43','01-02-2021');
 /*!40000 ALTER TABLE `Trades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,27 +156,27 @@ DROP TABLE IF EXISTS `Fills`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Fills` (
-  'FillID' int(11) UNIQUE NOT NULL AUTO_INCREMENT,
-  'TradeID' int(11) NOT UNIQUE NOT NULL,
-  'SecuritySymbol' varchar(255) NOT UNIQUE NOT NULL,
-  'Amount' int(11) NOT UNIQUE NOT NULL,
-  'Time' Time, NOT UNIQUE, NOT NULL,
-  'Date' Date, NOT UNIQUE, NOT NULL,
+  `FillID` int(11) UNIQUE NOT NULL,
+  `TradeID` int(11) NOT NULL,
+  `SecuritySymbol` varchar(255) NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `Time` Time NOT NULL,
+  `Date` Date NOT NULL,
   PRIMARY KEY (`FillID`),
-  CONSTRAINT 'TradesTradeID' FOREIGN KEY (`TradeID`) REFERENCES `Trades` (`TradeID`),
-  CONSTRAINT 'TradesSecuritySymbol' FOREIGN KEY (`SecuritySymbol`) REFERENCES `Securities` (`SecuritySymbol`)
+  CONSTRAINT `FillsTradeID` FOREIGN KEY (`TradeID`) REFERENCES `Trades` (`TradeID`),
+  CONSTRAINT `FillsSecuritySymbol` FOREIGN KEY (`SecuritySymbol`) REFERENCES `Securities` (`SecuritySymbol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Fills`
+-- Dumping data for Fills `Trades`
 --
 
 LOCK TABLES `Fills` WRITE;
 /*!40000 ALTER TABLE `Fills` DISABLE KEYS */;
+INSERT INTO `Fills` VALUES (1,1,'AAPL','500','9:30:35','01-01-2021'),(2,1,'AAPL','250','9:31:50','01-01-2021'),(3,2,'AMZN','350','9:35:57','01-01-2021');
 /*!40000 ALTER TABLE `Fills` ENABLE KEYS */;
 UNLOCK TABLES;
-
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
