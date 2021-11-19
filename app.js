3d579900bc5833ccf6abe31dc725a28fa9602b96
Austin Chap <chapa@flip2.engr.oregonstate.edu>
2021-11-18 19:01:19 -0800

    Updated app.js file to match main.js from example repo

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 /*
     SETUP
 */
-const express = require('express');   // We are using the express library for the web server
-const app     = express();            // We need to instantiate an express object to interact with the server in our code
-const PORT    = 5757;                 // Set a port number at the top so it's easy to change in the future
+var express = require('express');   // We are using the express library for the web server
+var mysql = require('./dbcon.js');
+
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+var PORT    = 5757;                 // Set a port number at the top so it's easy to change in the future
 
 app.use(express.urlencoded({
     extended: true
@@ -18,8 +20,15 @@ app.use(express.urlencoded({
 //     {
 //         res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
 //     });                                         // requesting the web site.
-
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
+app.set('mysql', mysql);
+app.use('/traders', require('./traders.js'));
+app.use('/managers', require('./managers.js'));
+app.use('/brokers', require('./brokers.js'));
+app.use('/securities', require('./securities.js'));
+app.use('/trades', require('./trades.js'));
+app.use('/fills', require('./fills.js'));
+app.use('/', express.static('public'));
 
 /*npm
     LISTENER
