3f59d353f9594b4c8dd8a94cb6a4686e00b67df3
Albert Chap <chapal@flip2.engr.oregonstate.edu>
2021-11-18 19:34:02 -0800

    added getBroker function

--- brokers.js
+++ brokers.js
@@ -2,7 +2,20 @@ module.exports = function(){
     const express = require('express');
     const router = express.Router();
 
-    
+    function getBrokers(res, mysql, context, complete){
+        mysql.pool.query("SELECT BrokerID as BrokerID, BrokerName, BrokerAddress FROM Brokers", function(error, results, fields){
+            if (error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.Brokers = results;
+            complete()
+        })
+    }
+
+    // function getBrokerAddress()
+
+
 
 
 
@@ -13,8 +26,8 @@ module.exports = function(){
         var context = {};
         context.jsscripts = ["deleteBrokers.js","filterBrokers.js","SearchFunctions.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
-        getPlanets(res, mysql, context, complete);
+        getBrokers(res, mysql, context, complete);
+        // getPlanets(res, mysql, context, complete);
         function complete(){
             callbackCount++;
             if(callbackCount >= 2){
